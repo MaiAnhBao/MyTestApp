05cc21122255c89f74d746a086feb6194b66d31d
hnnguyen <chuyendungdangki@yahoo.com>
2016-08-04 13:46:29 +0700

    Add onResume, onPause

--- app/src/main/java/com/example/hnnguyen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hnnguyen/myapplication/MainActivity.java
@@ -16,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        Toast.makeText(MainActivity.this, "onResume", Toast.LENGTH_SHORT).show();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(MainActivity.this, "onPause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
         Log.d(TAG,"onTouchEvent");
         float eventX = event.getX();
