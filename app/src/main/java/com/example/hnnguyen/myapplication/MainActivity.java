fe2eb60db8fe73fb7dbc29e6e5071e95460025af
hnnguyen <chuyendungdangki@yahoo.com>
2016-09-01 14:35:08 +0700

    add Gradle

--- app/src/main/java/com/example/hnnguyen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hnnguyen/myapplication/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.hnnguyen.myapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Socket socket;
+    private String serverIPAddr = "192.168.81.102";
+    private static final int SERVERPORT = 8082;
+    public PrintWriter out;
 
     public static final String TAG = "Test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Client client = new Client();
+//        new Thread(new client.ClientThread()).run();
+        new AsyncAction().execute("");
     }
 
     @Override
@@ -49,5 +66,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+    private class AsyncAction extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                InetAddress inetAddress = InetAddress.getByName(serverIPAddr);
+                socket = new Socket(inetAddress,SERVERPORT);
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
